71de430c4bb9a23b20280bc719e9e87106537c64
Michael Pavich <mpavich2@my.westga.edu>
2021-04-27 21:56:59 -0400

    Added some default view pages and controller for the products, contact, and about pages.

---
+++ MichaelPavichFinal/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MichaelPavichFinal.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
