cc246de24770f1dabe54b6fbf7d4e19607d3f481
Michael Pavich <mpavich2@my.westga.edu>
2021-05-01 19:09:05 -0400

    Added the cart view classes.

---
+++ MichaelPavichFinal/Models/ViewModels/CartViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace MichaelPavichFinal.Models
+{
+    public class CartViewModel 
+    {
+        public IEnumerable<CartItem> List { get; set; }
+        public double Subtotal { get; set; }
+        public RouteDictionary BookGridRoute { get; set; }
+    }
+}
